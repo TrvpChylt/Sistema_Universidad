3285f49dbd2785bee879d1e8577c4992afc5a086
chylt <Jesus.cr2909@gmail.com>
2026-01-18 14:06:10 -0400

    Modificacion para agregar las materias

--- init_db.py
+++ init_db.py
@@ -1,15 +1,34 @@
 import sqlite3
 
 connection = sqlite3.connect('database.db')
+
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
-# Insertamos las 3 carreras iniciales
+
 cur.execute("INSERT INTO carreras_universitarias (nombre_carrera) VALUES (?)", ('Ingeniería en Informática',))
+id_informatica = cur.lastrowid 
+
 cur.execute("INSERT INTO carreras_universitarias (nombre_carrera) VALUES (?)", ('Licenciatura en Administración',))
+id_administracion = cur.lastrowid 
+
 cur.execute("INSERT INTO carreras_universitarias (nombre_carrera) VALUES (?)", ('Derecho',))
+id_derecho = cur.lastrowid
+
+materias = [
+    ('Programación I', id_informatica),
+    ('Base de Datos', id_informatica),
+    ('Análisis Matemático', id_informatica),
+    ('Contabilidad I', id_administracion),
+    ('Microeconomía', id_administracion),
+    ('Derecho Civil I', id_derecho),
+    ('Introducción al Derecho', id_derecho)
+]
+
+cur.executemany("INSERT INTO materias (nombre_materia, carrera_id) VALUES (?, ?)", materias)
 
 connection.commit()
 connection.close()
-print("Base de datos SQLite lista.")
+
+print("Base de datos SQLite lista con carreras y materias vinculadas.")
