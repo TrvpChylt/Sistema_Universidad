84ac748dc21bf99a549205aa8a0adf9109cd870c
chylt <Jesus.cr2909@gmail.com>
2026-01-25 17:28:23 -0400

    Limpieza de schema.sql y unificación de inserts en init_db.py

--- init_db.py
+++ init_db.py
@@ -2,11 +2,13 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
+# 1. Ejecuta el esquema (solo creación de tablas)
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
+# 2. Insertar Carreras
 cur.execute("INSERT INTO carreras_universitarias (nombre_carrera) VALUES (?)", ('Ingeniería en Informática',))
 id_informatica = cur.lastrowid 
 
@@ -16,6 +18,7 @@ id_administracion = cur.lastrowid
 cur.execute("INSERT INTO carreras_universitarias (nombre_carrera) VALUES (?)", ('Derecho',))
 id_derecho = cur.lastrowid
 
+# 3. Lista de Materias
 materias = [
     ('Programación I', id_informatica),
     ('Ingles', id_informatica),
@@ -28,7 +31,9 @@ materias = [
     ('Introducción al Derecho', id_derecho)
 ]
 
-cur.executemany("INSERT INTO materias (nombre_materia, carrera_id) VALUES (?, ?)", materias)
+# 4. Insertar Materias (Cambiado carrera_id por id_carrera para que coincida con tu SQL)
+cur.executemany("INSERT INTO materias (nombre_materia, id_carrera) VALUES (?, ?)", materias)
 
 connection.commit()
 connection.close()
+print("¡Base de datos creada y poblada con éxito!")
